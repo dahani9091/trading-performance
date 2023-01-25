655ac8362cabb80efb4c06ecd0fe329b6653acd4
Adel Dahani <69170574+dahani9091@users.noreply.github.com>
2023-01-25 16:34:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ The api_base_url parameter in the Mastodon client initialization is the base URL
 For example, if your Mastodon account is on the "mastodon.social" instance, the api_base_url would be https://mastodon.social.
 '''
 
-mastodon_access_token = '2hdmjp5510Kg_XrWPVgTqgTHnmEvP7l3IK1TyZ2KY2Y'
+mastodon_access_token = ''
 mastodon_api_base_url = 'https://arsenalfc.social/'
 stocktwits_username = 'dhn9091'
 
